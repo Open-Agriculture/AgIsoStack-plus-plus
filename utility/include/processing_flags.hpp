839f362d202ef23ecef06244df96ba9f8b66831d
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2022-09-03 11:27:04 -0600

    [VT]: Add processing flags class
    This class uses a bitfield to keep track of any number of arbitrary flags.
    Good for knowing when you need to send a CAN message and retries.

---
+++ utility/include/processing_flags.hpp
@@ -0,0 +1,35 @@
+//================================================================================================
+/// @file processing_flags.hpp
+///
+/// @brief A class that manages 1 bit flags. Handy as a retry machanism for sending CAN messages.
+/// @author Adrian Del Grosso
+///
+/// @copyright 2022 Adrian Del Grosso
+//================================================================================================
+#ifndef PROCESSING_FLAGS_HPP
+#define PROCESSING_FLAGS_HPP
+
+#include <cstdint>
+
+namespace isobus
+{
+	class ProcessingFlags
+	{
+	public:
+		typedef void (*ProcessFlagsCallback)(std::uint32_t flag, void *parentPointer);
+
+		ProcessingFlags(std::uint32_t numberOfFlags, ProcessFlagsCallback processingCallback, void *parentPointer);
+		~ProcessingFlags();
+
+		void set_flag(std::uint32_t flag);
+		void process_all_flags();
+
+	private:
+		ProcessFlagsCallback callback;
+		const std::uint32_t maxFlag;
+		std::uint8_t *flagBitfield;
+		void *parent;
+	};
+} // namespace isobus
+
+#endif // PROCESSING_FLAGS_HPP
---
+++ utility/src/processing_flags.cpp
@@ -0,0 +1,62 @@
+//================================================================================================
+/// @file processing_flags.cpp
+///
+/// @brief A class that manages 1 bit flags. Handy as a retry machanism for sending CAN messages.
+/// @author Adrian Del Grosso
+///
+/// @copyright 2022 Adrian Del Grosso
+//================================================================================================
+
+#include "processing_flags.hpp"
+
+#include <cstring>
+
+namespace isobus
+{
+	ProcessingFlags::ProcessingFlags(std::uint32_t numberOfFlags, ProcessFlagsCallback processingCallback, void *parentPointer) :
+	  callback(processingCallback),
+	  maxFlag(numberOfFlags),
+	  flagBitfield(nullptr),
+	  parent(parentPointer)
+	{
+		const std::uint32_t numberBytes = (numberOfFlags / 8) + 1;
+		flagBitfield = new std::uint8_t[numberBytes];
+		memset(flagBitfield, 0, numberBytes);
+	}
+
+	ProcessingFlags ::~ProcessingFlags()
+	{
+		delete[] flagBitfield;
+		flagBitfield = nullptr;
+	}
+
+	void ProcessingFlags::set_flag(std::uint32_t flag)
+	{
+		if (flag <= maxFlag)
+		{
+			flagBitfield[(flag / 8)] |= (1 << (flag % 8));
+		}
+	}
+
+	void ProcessingFlags::process_all_flags()
+	{
+		const std::uint32_t numberBytes = (maxFlag / 8) + 1;
+
+		for (std::uint32_t i = 0; i < numberBytes; i++)
+		{
+			if (flagBitfield[i])
+			{
+				for (std::uint8_t j = 0; j < 8; j++)
+				{
+					std::uint8_t currentFlag = (flagBitfield[i] & (1 << j));
+
+					if (currentFlag)
+					{
+						flagBitfield[i] &= ~(currentFlag);
+						callback((8 * i) + j, parent);
+					}
+				}
+			}
+		}
+	}
+} // namespace isobus
