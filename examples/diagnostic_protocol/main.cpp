6a0cea3f20508967f1061c7f853821f873dbbc21
Ryan Friedman <ryan_friedman@trimble.com>
2022-11-15 18:22:14 -0700

    Rework includes to support multi-level includes
    * When installed, need the installed headers to support local includes (can remove later once includes are refactored)
    * When building, still support local includes
    * Recommend using full include paths

--- examples/diagnostic_protocol/main.cpp
+++ examples/diagnostic_protocol/main.cpp
@@ -1,8 +1,8 @@
-#include "can_general_parameter_group_numbers.hpp"
-#include "can_hardware_interface.hpp"
-#include "can_network_manager.hpp"
-#include "isobus_diagnostic_protocol.hpp"
-#include "socket_can_interface.hpp"
+#include "isobus/isobus/can_general_parameter_group_numbers.hpp"
+#include "isobus/hardware_integration/can_hardware_interface.hpp"
+#include "isobus/isobus/can_network_manager.hpp"
+#include "isobus/isobus/isobus_diagnostic_protocol.hpp"
+#include "isobus/hardware_integration/socket_can_interface.hpp"
 
 #include <csignal>
 #include <iterator>
