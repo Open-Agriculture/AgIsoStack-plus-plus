f83317a6e6ed796a8e4bf2ccc65625bef187162e
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2022-12-24 17:16:49 -0700

    [VT]: Add optional, automatic load/store for object pools
    Also added a utility function to hash an object pool to generate a somewhat random version label.

--- utility/include/isobus/utility/iop_file_interface.hpp
+++ utility/include/isobus/utility/iop_file_interface.hpp
@@ -19,7 +19,8 @@ namespace isobus
 	//================================================================================================
 	/// @class IOPFileInterface
 	///
-	/// @brief A class that manages reading IOP files
+	/// @brief A class that manages reading IOP files and provides a simple way to generate
+	/// unique versions.
 	/// @details IOP files are the standard format for ISOBUS object pools. This class will read
 	/// an IOP file, and return a vector that represents the object pool stored in the file.
 	//================================================================================================
@@ -30,6 +31,12 @@ namespace isobus
 		/// @param[in] filename A string filepath for the IOP file to read
 		/// @returns A vector with an object pool in it, or an empty vector if reading failed
 		static std::vector<std::uint8_t> read_iop_file(const std::string &filename);
+
+		/// @brief Reads an object pool and generates a string version by hashing it
+		/// @details Credit for the hash algorithm here goes to "see" on stack overflow.
+		/// @param[in] iopData The object pool to hash and generate a version for
+		/// @returns A 7 character string that is probably somewhat unique for this pool
+		static std::string hash_object_pool_to_version(std::vector<std::uint8_t> &iopData);
 	};
 }
 
