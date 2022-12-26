d49504eff9c7d991990504c780bba3fcf1c323a7
GwnDaan <thadutchgameryt@gmail.com>
2022-12-26 23:23:38 +0100

    [Core]: Fix initialization of mcp2515 plugin
    Also switched to std::vector instead of c arrays as it's probably more secure in this case

--- hardware_integration/src/mcp2515_can_interface.cpp
+++ hardware_integration/src/mcp2515_can_interface.cpp
@@ -7,6 +7,7 @@
 /// @copyright 2022 Adrian Del Grosso
 //================================================================================================
 #include "isobus/hardware_integration/mcp2515_can_interface.hpp"
+#include "isobus/utility/system_timing.hpp"
 
 MCP2515CANInterface::MCP2515CANInterface(SPITransmissionCallback callback, const std::uint8_t cfg1, const std::uint8_t cfg2, const std::uint8_t cfg3) :
   callback(callback),
@@ -33,13 +34,19 @@ void MCP2515CANInterface::open()
 {
 	if (callback != nullptr)
 	{
-		if (set_mode(MCPMode::CONFIG))
+		if (reset())
 		{
-			if (write_register(MCPRegister::CNF1, cfg1) &&
-			    write_register(MCPRegister::CNF2, cfg2) &&
-			    write_register(MCPRegister::CNF3, cfg3))
+			if (set_mode(MCPMode::CONFIG))
 			{
-				initialized = true;
+				if (write_register(MCPRegister::CNF1, cfg1) &&
+				    write_register(MCPRegister::CNF2, cfg2) &&
+				    write_register(MCPRegister::CNF3, cfg3))
+				{
+					if (set_mode(MCPMode::NORMAL))
+					{
+						initialized = true;
+					}
+				}
 			}
 		}
 	}
@@ -48,29 +55,29 @@ void MCP2515CANInterface::open()
 bool MCP2515CANInterface::reset()
 {
 	bool retVal = false;
-	if (write_instruction(MCPInstruction::RESET, 1))
+	if (write_instruction(MCPInstruction::RESET))
 	{
 		// Depends on oscillator & capacitors used
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-		uint8_t zeros[14] = { 0 };
-		if (write_register(MCPRegister::TXB0CTRL, zeros, 14) &&
-		    write_register(MCPRegister::TXB1CTRL, zeros, 14) &&
-		    write_register(MCPRegister::TXB2CTRL, zeros, 14) &&
+		std::vector<std::uint8_t> zeros(14, 0);
+		if (write_register(MCPRegister::TXB0CTRL, zeros) &&
+		    write_register(MCPRegister::TXB1CTRL, zeros) &&
+		    write_register(MCPRegister::TXB2CTRL, zeros) &&
 		    write_register(MCPRegister::RXB0CTRL, 0) &&
 		    write_register(MCPRegister::RXB1CTRL, 0) &&
 		    write_register(MCPRegister::CANINTE, 0xA3))
 		{
 			retVal = true;
 		}
-		return retVal;
 	}
+	return retVal;
 }
 
 std::uint8_t MCP2515CANInterface::get_rx_status() const
 {
 	std::uint8_t retVal = 0;
-	if (get_is_valid())
+	if (callback != nullptr)
 	{
 		const std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(MCPInstruction::RX_STATUS), 0x00 };
 		std::vector<std::uint8_t> response;
@@ -84,54 +91,60 @@ std::uint8_t MCP2515CANInterface::get_rx_status() const
 bool MCP2515CANInterface::read_register(const MCPRegister address, std::uint8_t &data)
 {
 	bool retVal = false;
-	std::uint8_t rxData[1];
-	if (read_register(address, rxData, 1))
+	if (callback != nullptr)
 	{
-		data = rxData[0];
-		retVal = true;
-	}
-	return retVal;
-}
-
-bool MCP2515CANInterface::read_register(const MCPRegister address, std::uint8_t data[], const std::size_t length)
-{
-	bool retVal = false;
-	if (get_is_valid())
-	{
-		std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(MCPInstruction::READ), static_cast<uint8_t>(address) };
-		txData.resize(length + 2);
+		std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(MCPInstruction::READ), static_cast<uint8_t>(address), 0 };
 		std::vector<std::uint8_t> rxData;
 
-		if (callback(txData, rxData) && rxData.size() == length + 2)
+		if (callback(txData, rxData) && rxData.size() == 3)
 		{
-			for (std::size_t i = 2; i < length + 2; i++)
-			{
-				data[i - 2] = rxData[i];
-			}
+			data = rxData[2];
 			retVal = true;
 		}
 	}
 	return retVal;
 }
-bool MCP2515CANInterface::write_instruction(const MCPInstruction instruction, const std::uint8_t data)
+
+bool MCP2515CANInterface::read_register(const MCPRegister address, std::vector<std::uint8_t> &data, const std::size_t length)
 {
 	bool retVal = false;
-	if (write_instruction(instruction, &data, 1))
+	if (callback != nullptr)
+	{
+		std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(MCPInstruction::READ), static_cast<uint8_t>(address) };
+		txData.resize(length + 2);
+		std::vector<std::uint8_t> rxData;
+
+		if (callback(txData, rxData) && rxData.size() == data.size() + 2)
+		{
+			memcpy(data.data(), rxData.data() + 2, data.size());
+			retVal = true;
+		}
+	}
+	return retVal;
+}
+bool MCP2515CANInterface::write_instruction(const MCPInstruction instruction)
+{
+	bool retVal = false;
+	std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(instruction) };
+	std::vector<std::uint8_t> rxData;
+	if (callback(txData, rxData) && 1 == rxData.size())
 	{
 		retVal = true;
 	}
 	return retVal;
 }
 
-bool MCP2515CANInterface::write_instruction(const MCPInstruction instruction, const std::uint8_t data[], const std::size_t length)
+bool MCP2515CANInterface::write_instruction(const MCPInstruction instruction, const std::vector<std::uint8_t> data)
 {
 	bool retVal = false;
-	if (get_is_valid())
+	if (callback != nullptr)
 	{
 		std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(instruction) };
-		txData.insert(txData.end(), data, data + length);
+		txData.insert(std::end(txData), std::begin(data), std::end(data));
+
 		std::vector<std::uint8_t> rxData;
-		if (callback(txData, rxData) && rxData.size() == length + 1)
+		auto result = callback(txData, rxData);
+		if (result && rxData.size() == data.size() + 1)
 		{
 			retVal = true;
 		}
@@ -142,8 +155,8 @@ bool MCP2515CANInterface::write_instruction(const MCPInstruction instruction, co
 bool MCP2515CANInterface::modify_register(const MCPRegister address, const std::uint8_t mask, const std::uint8_t data)
 {
 	bool retVal = false;
-	std::uint8_t txData[] = { static_cast<uint8_t>(address), mask, data };
-	if (write_instruction(MCPInstruction::BITMOD, txData, 3))
+	std::vector<std::uint8_t> txData = { static_cast<uint8_t>(address), mask, data };
+	if (write_instruction(MCPInstruction::BITMOD, txData))
 	{
 		retVal = true;
 	}
@@ -153,47 +166,44 @@ bool MCP2515CANInterface::modify_register(const MCPRegister address, const std::
 bool MCP2515CANInterface::write_register(const MCPRegister address, const std::uint8_t data)
 {
 	bool retVal = false;
-	if (write_register(address, &data, 1))
+	std::vector<std::uint8_t> dataVector = { data };
+	if (write_register(address, dataVector))
 	{
 		retVal = true;
 	}
 	return retVal;
 }
 
-bool MCP2515CANInterface::write_register(const MCPRegister address, const std::uint8_t data[], const std::size_t length)
+bool MCP2515CANInterface::write_register(const MCPRegister address, const std::vector<std::uint8_t> data)
 {
 	bool retVal = false;
-	if (write_instruction(MCPInstruction::WRITE, (static_cast<uint8_t>(address), data), length + 1))
+	std::vector<std::uint8_t> txData = { static_cast<uint8_t>(address) };
+	txData.insert(std::end(txData), std::begin(data), std::end(data));
+
+	if (write_instruction(MCPInstruction::WRITE, txData))
 	{
 		retVal = true;
 	}
-	// if (get_is_valid())
-	// {
-	// 	std::vector<std::uint8_t> txData = { static_cast<std::uint8_t>(MCPInstruction::WRITE), static_cast<uint8_t>(address) };
-	// 	txData.insert(txData.end(), data, data + length);
-	// 	std::vector<std::uint8_t> rxData;
-	// 	if (callback(txData, rxData) && rxData.size() == length + 2)
-	// 	{
-	// 		retVal = true;
-	// 	}
-	// }
 	return retVal;
 }
 
 bool MCP2515CANInterface::set_mode(const MCPMode mode)
 {
 	bool retVal = false;
-	if (modify_register(MCPRegister::CANCTRL, 1, static_cast<std::uint8_t>(mode)))
+	if (modify_register(MCPRegister::CANCTRL, 0xE0, static_cast<std::uint8_t>(mode)))
 	{
 		// Wait for the mode to be set within 10ms
-		const auto start = std::chrono::steady_clock::now();
-		while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() < 10)
+		const auto start = isobus::SystemTiming::get_timestamp_ms();
+		while (isobus::SystemTiming::get_time_elapsed_ms(start) < 10)
 		{
 			std::uint8_t newMode;
-			if (read_register(MCPRegister::CANSTAT, newMode) && (newMode & 0xE0) == static_cast<std::uint8_t>(mode))
+			if (read_register(MCPRegister::CANSTAT, newMode))
 			{
-				retVal = true;
-				break;
+				if ((newMode & 0xE0) == static_cast<std::uint8_t>(mode))
+				{
+					retVal = true;
+					break;
+				}
 			}
 		}
 	}
@@ -202,23 +212,35 @@ bool MCP2515CANInterface::set_mode(const MCPMode mode)
 
 bool MCP2515CANInterface::read_frame(isobus::HardwareInterfaceCANFrame &canFrame)
 {
+	constexpr static std::array<RXBRegister, NUM_READ_BUFFERS> RXB = { { { MCPRegister::RXB0CTRL, MCPRegister::RXB0DATA, 0x01 },
+		                                                                   { MCPRegister::RXB1CTRL, MCPRegister::RXB1DATA, 0x02 } } }; ///< The registers of the RX buffers
 	bool retVal = false;
-	uint8_t status = get_rx_status();
 
-	// Check if any of the buffers have a message
+	// Wait for message to be available
 	const struct RXBRegister *rxb;
-	if (status & 0x01)
+
+	while (true)
 	{
-		rxb = &RXB[0];
-	}
-	else if (status & 0x02)
-	{
-		rxb = &RXB[1];
+		const std::uint8_t status = get_rx_status();
+
+		// Check if any of the buffers have a message
+		if (status & 0x01)
+		{
+			rxb = &RXB[0];
+			break;
+		}
+		else if (status & 0x02)
+		{
+			rxb = &RXB[1];
+			break;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(RECEIVE_MESSAGE_READ_RATE));
 	}
 
-	if (rxb != nullptr)
+	if (rxb)
 	{
-		std::uint8_t rxData[6];
+		std::vector<std::uint8_t> rxData;
 		if (read_register(rxb->ctrl, rxData, 6))
 		{
 			uint32_t id = (rxData[1] << 3) + (rxData[2] >> 5);
@@ -242,8 +264,7 @@ bool MCP2515CANInterface::read_frame(isobus::HardwareInterfaceCANFrame &canFrame
 			{
 				retVal = true;
 
-				uint8_t data[CAN_MAX_DLEN];
-				if (read_register(rxb->data, data, len) &&
+				if (read_register(rxb->data, rxData, 8) &&
 				    modify_register(MCPRegister::CANINTF, rxb->intf, 0) &&
 				    0 == (id & CAN_ERR_FLAG))
 				{
@@ -259,7 +280,7 @@ bool MCP2515CANInterface::read_frame(isobus::HardwareInterfaceCANFrame &canFrame
 					}
 					canFrame.dataLength = len;
 					memset(canFrame.data, 0, sizeof(canFrame.data));
-					memcpy(canFrame.data, data, canFrame.dataLength);
+					memcpy(canFrame.data, rxData.data(), canFrame.dataLength);
 
 					// Timestamp is not supported by MCP2515
 
@@ -274,6 +295,11 @@ bool MCP2515CANInterface::read_frame(isobus::HardwareInterfaceCANFrame &canFrame
 
 bool MCP2515CANInterface::write_frame(const isobus::HardwareInterfaceCANFrame &canFrame)
 {
+	// The register addresses for the TX buffers
+	constexpr static std::array<TXBRegister, NUM_WRITE_BUFFERS> TXB = { { { MCPRegister::TXB0CTRL, MCPRegister::TXB0SIDH, MCPRegister::TXB0DATA },
+		                                                                    { MCPRegister::TXB1CTRL, MCPRegister::TXB1SIDH, MCPRegister::TXB1DATA },
+		                                                                    { MCPRegister::TXB2CTRL, MCPRegister::TXB2SIDH, MCPRegister::TXB2DATA } } };
+
 	bool retVal = false;
 
 	for (int i = 0; i < NUM_WRITE_BUFFERS; i++)
@@ -284,7 +310,7 @@ bool MCP2515CANInterface::write_frame(const isobus::HardwareInterfaceCANFrame &c
 		    ctrl & 0x08)
 		{
 			// Buffer can be written to
-			uint8_t data[13];
+			std::vector<uint8_t> data(13, 0);
 
 			if (canFrame.isExtendedFrame)
 			{
@@ -304,7 +330,7 @@ bool MCP2515CANInterface::write_frame(const isobus::HardwareInterfaceCANFrame &c
 			data[4] = canFrame.dataLength; //< DLC
 			memcpy(&data[5], canFrame.data, canFrame.dataLength); //< Data
 
-			if ((write_register(txbuf->sidh, data, 5 + canFrame.dataLength)) &&
+			if ((write_register(txbuf->sidh, data)) &&
 			    (modify_register(txbuf->ctrl, 0x08, 0x08)) &&
 			    (read_register(txbuf->ctrl, ctrl)) &&
 			    ((ctrl & (0x40 | 0x20 | 0x10)) == 0))
