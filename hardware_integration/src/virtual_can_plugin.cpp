0eeba9fa18454f43981e82020739395bc0c5dfa4
GwnDaan <d.steenbergen@student.tue.nl>
2023-01-28 01:00:14 +0100

    [Core]: Add virtual CAN driver plugin

---
+++ hardware_integration/src/virtual_can_plugin.cpp
@@ -0,0 +1,73 @@
+//================================================================================================
+/// @file virtual_can_plugin.cpp
+///
+/// @brief A driver for a virtual CAN bus that can be used for (automated) testing.
+/// @author Daan Steenbergen
+///
+/// @copyright 2023 Adrian Del Grosso
+//================================================================================================
+#include "isobus/hardware_integration/virtual_can_plugin.hpp"
+
+std::mutex VirtualCANPlugin::mutex;
+std::map<std::string, std::vector<VirtualCANPlugin::VirtualDevice>> VirtualCANPlugin::channels;
+
+VirtualCANPlugin::VirtualCANPlugin(const std::string channel, const bool receiveOwnMessages) :
+  channel(channel),
+  receiveOwnMessages(receiveOwnMessages)
+{
+	const std::lock_guard<std::mutex> lock(mutex);
+	channels[channel].push_back({ {}, this });
+	ourDevice = &channels[channel].back();
+}
+
+bool VirtualCANPlugin::get_is_valid() const
+{
+	return true;
+}
+
+std::string VirtualCANPlugin::get_channel_name() const
+{
+	return channel;
+}
+
+void VirtualCANPlugin::open()
+{
+	running = true;
+}
+
+void VirtualCANPlugin::close()
+{
+	running = false;
+}
+
+bool VirtualCANPlugin::write_frame(const isobus::HardwareInterfaceCANFrame &canFrame)
+{
+	bool retVal = false;
+	const std::lock_guard<std::mutex> lock(mutex);
+	for (VirtualDevice &device : channels[channel])
+	{
+		if (device.queue.size() < MAX_QUEUE_SIZE)
+		{
+			if (receiveOwnMessages || device.owner != this)
+			{
+				device.queue.push_back(canFrame);
+				device.owner->condition.notify_one();
+				retVal = true;
+			}
+		}
+	}
+	return retVal;
+}
+
+bool VirtualCANPlugin::read_frame(isobus::HardwareInterfaceCANFrame &canFrame)
+{
+	std::unique_lock<std::mutex> lock(mutex);
+	condition.wait(lock, [this] { return !running || !ourDevice->queue.empty(); });
+	if (!ourDevice->queue.empty())
+	{
+		canFrame = ourDevice->queue.front();
+		ourDevice->queue.pop_front();
+		return true;
+	}
+	return false;
+}
