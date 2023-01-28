c0390eb5e6515b6df03e32a156ca46d7a77aa895
GwnDaan <d.steenbergen@student.tue.nl>
2023-01-28 01:15:00 +0100

    [Core]: Add InnoMaker USB2CAN plugin Make sure to add the `InnoMakerUsb2CanLib.dll` file to the directory you are running the executable from

---
+++ hardware_integration/include/isobus/hardware_integration/innomaker_usb2can_windows_plugin.hpp
@@ -0,0 +1,92 @@
+//================================================================================================
+/// @file innomaker_usb2can_plugin.hpp
+///
+/// @brief An interface for using an InnoMaker USB2CAN device.
+/// @attention This is not legal advice. The InnoMaker USB2CAN driver uses libusb, which is
+/// an LGPL-2.1 library. Be sure you understand the implications of this before proceeding.
+/// @author Adrian Del Grosso
+///
+/// @copyright 2022 Adrian Del Grosso
+//================================================================================================
+#ifndef INNOMAKER_USB2CAN_PLUGIN_HPP
+#define INNOMAKER_USB2CAN_PLUGIN_HPP
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "isobus/hardware_integration/InnoMakerUsb2CanLib.h"
+#include "isobus/hardware_integration/can_hardware_plugin.hpp"
+#include "isobus/isobus/can_frame.hpp"
+#include "isobus/isobus/can_hardware_abstraction.hpp"
+
+//================================================================================================
+/// @class InnomakerUSB2CANWindowsPlugin
+///
+/// @brief A Windows CAN Driver for InnoMaker USB2CAN devices
+//================================================================================================
+class InnomakerUSB2CANWindowsPlugin : public CANHardwarePlugin
+{
+public:
+	/// @brief The baudrates supported by the InnoMaker USB2CAN device
+	enum Baudrate
+	{
+		B20k, ///< 20 kbps baudrate
+		B33k3, ///< 33.3 kbps baudrate
+		B40k, ///< 40 kbps baudrate
+		B50k, ///< 50 kbps baudrate
+		B66k6, ///< 66.6 kbps baudrate
+		B80k, ///< 80 kbps baudrate
+		B83k3, ///< 83.3 kbps baudrate
+		B100k, ///< 100 kbps baudrate
+		B125k, ///< 125 kbps baudrate
+		B200k, ///< 200 kbps baudrate
+		B250k, ///< 250 kbps baudrate
+		B400k, ///< 400 kbps baudrate
+		B500k, ///< 500 kbps baudrate
+		B666k, ///< 666 kbps baudrate
+		B800k, ///< 800 kbps baudrate
+		B1000k, ///< 1000 kbps baudrate
+	};
+
+	/// @brief Constructor for the Windows version of the Innomaker USB2CAN Windows CAN driver
+	/// @param[in] channel The channel to use by index, passed directly to the consuming driver
+	explicit InnomakerUSB2CANWindowsPlugin(int channel, Baudrate baudrate = B250k);
+
+	/// @brief The destructor for InnomakerUSB2CANWindowsPlugin
+	virtual ~InnomakerUSB2CANWindowsPlugin();
+
+	/// @brief Returns if the connection with the hardware is valid
+	/// @returns `true` if connected, `false` if not connected
+	bool get_is_valid() const override;
+
+	/// @brief Closes the connection to the hardware
+	void close() override;
+
+	/// @brief Connects to the hardware you specified in the constructor's channel argument
+	void open() override;
+
+	/// @brief Returns a frame from the hardware (synchronous), or `false` if no frame can be read.
+	/// @param[in, out] canFrame The CAN frame that was read
+	/// @returns `true` if a CAN frame was read, otherwise `false`
+	bool read_frame(isobus::HardwareInterfaceCANFrame &canFrame) override;
+
+	/// @brief Writes a frame to the bus (synchronous)
+	/// @param[in] canFrame The frame to write to the bus
+	/// @returns `true` if the frame was written, otherwise `false`
+	bool write_frame(const isobus::HardwareInterfaceCANFrame &canFrame) override;
+
+private:
+	static constexpr InnoMakerUsb2CanLib::UsbCanMode CAN_MODE = InnoMakerUsb2CanLib::UsbCanModeNormal; ///< The mode to use for the CAN device
+	static constexpr std::uint32_t CAN_EFF_FLAG = 0x80000000; ///< Set if the frame is extended
+	static constexpr std::uint32_t CAN_SFF_MASK = 0x000007FF; ///< The mask for standard frames
+	static constexpr std::uint32_t CAN_EFF_MASK = 0x1FFFFFFF; ///< The mask for extended frames
+
+	static std::unique_ptr<InnoMakerUsb2CanLib>
+	  driverInstance; ///< The driver itself
+	const int channel; ///< Stores the channel associated with this object
+	const std::uint32_t baudrate; ///< Stores the baud rate associated with this object
+	std::unique_ptr<InnoMakerUsb2CanLib::innomaker_can> txContexts; /// Stores Tx tickets for the driver
+};
+
+#endif // INNOMAKER_USB2CAN_PLUGIN_HPP
---
+++ hardware_integration/src/innomaker_usb2can_windows_plugin.cpp
@@ -0,0 +1,337 @@
+//================================================================================================
+/// @file innomaker_usb2can_plugin.cpp
+///
+/// @brief An interface for using an INNO-Maker USB2CAN
+/// @attention Use of this plugin may affect your license, as the LGPL-2.1 libusb will be linked to.
+/// However, if you do not link to it, your executable should remain MIT.
+/// This is not legal advice though, and you should be sure you understand the implications
+/// of including this plugin.
+/// @author Adrian Del Grosso
+///
+/// @copyright 2023 Adrian Del Grosso
+//================================================================================================
+
+#include "isobus/hardware_integration/innomaker_usb2can_windows_plugin.hpp"
+#include "isobus/isobus/can_stack_logger.hpp"
+
+#include <thread>
+
+std::unique_ptr<InnoMakerUsb2CanLib> InnomakerUSB2CANWindowsPlugin::driverInstance = nullptr;
+
+InnomakerUSB2CANWindowsPlugin::InnomakerUSB2CANWindowsPlugin(int channel, Baudrate baudrate) :
+  channel(channel),
+  baudrate(baudrate)
+{
+	if (nullptr == driverInstance)
+	{
+		driverInstance = std::unique_ptr<InnoMakerUsb2CanLib>(new InnoMakerUsb2CanLib());
+		driverInstance->setup();
+		driverInstance->scanInnoMakerDevice();
+	}
+	txContexts = std::unique_ptr<InnoMakerUsb2CanLib::innomaker_can>(new InnoMakerUsb2CanLib::innomaker_can());
+}
+
+InnomakerUSB2CANWindowsPlugin::~InnomakerUSB2CANWindowsPlugin()
+{
+	close();
+}
+
+bool InnomakerUSB2CANWindowsPlugin::get_is_valid() const
+{
+	return nullptr != driverInstance->getInnoMakerDevice(channel) && driverInstance->getInnoMakerDevice(channel)->isOpen;
+}
+
+void InnomakerUSB2CANWindowsPlugin::close()
+{
+	InnoMakerUsb2CanLib::InnoMakerDevice *device = driverInstance->getInnoMakerDevice(channel);
+
+	if (nullptr != device)
+	{
+		driverInstance->urbResetDevice(device);
+		driverInstance->closeInnoMakerDevice(device);
+
+		// Check if all channels are closed and if so, close the driver instance
+		bool allChannelsClosed = true;
+		for (int i = 0; i < driverInstance->getInnoMakerDeviceCount(); i++)
+		{
+			if (nullptr != driverInstance->getInnoMakerDevice(i) &&
+			    driverInstance->getInnoMakerDevice(i)->isOpen)
+			{
+				allChannelsClosed = false;
+				break;
+			}
+		}
+		if (allChannelsClosed)
+		{
+			isobus::CANStackLogger::info("[Innomaker-Windows] All channels closed, closing driver instance");
+			driverInstance->setdown();
+			driverInstance = nullptr;
+		}
+	}
+}
+
+void InnomakerUSB2CANWindowsPlugin::open()
+{
+	InnoMakerUsb2CanLib::InnoMakerDevice *device = driverInstance->getInnoMakerDevice(channel);
+
+	if (nullptr != device)
+	{
+		driverInstance->urbResetDevice(device);
+		driverInstance->closeInnoMakerDevice(device);
+
+		// Reset tx buffer according to the documentation
+		for (int i = 0; i < driverInstance->innomaker_MAX_TX_URBS; i++)
+		{
+			txContexts->tx_context[i].echo_id = driverInstance->innomaker_MAX_TX_URBS;
+		}
+
+		InnoMakerUsb2CanLib::Innomaker_device_bittming bitTiming;
+		switch (baudrate)
+		{
+			case B20k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 150;
+			}
+			break;
+			case B33k3:
+			{
+				bitTiming.prop_seg = 3;
+				bitTiming.phase_seg1 = 3;
+				bitTiming.phase_seg2 = 1;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 180;
+			}
+			break;
+			case B40k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 75;
+			}
+			break;
+			case B50k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 60;
+			}
+			break;
+			case B66k6:
+			{
+				bitTiming.prop_seg = 3;
+				bitTiming.phase_seg1 = 3;
+				bitTiming.phase_seg2 = 1;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 90;
+			}
+			break;
+			case B80k:
+			{
+				bitTiming.prop_seg = 3;
+				bitTiming.phase_seg1 = 3;
+				bitTiming.phase_seg2 = 1;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 75;
+			}
+			break;
+			case B83k3:
+			{
+				bitTiming.prop_seg = 3;
+				bitTiming.phase_seg1 = 3;
+				bitTiming.phase_seg2 = 1;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 72;
+			}
+			break;
+			case B100k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 30;
+			}
+			break;
+			case B125k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 24;
+			}
+			break;
+			case B200k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 15;
+			}
+			break;
+			case B250k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 12;
+			}
+			break;
+			case B400k:
+			{
+				bitTiming.prop_seg = 3;
+				bitTiming.phase_seg1 = 3;
+				bitTiming.phase_seg2 = 1;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 15;
+			}
+			break;
+			case B500k:
+			{
+				bitTiming.prop_seg = 6;
+				bitTiming.phase_seg1 = 7;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 6;
+			}
+			break;
+			case B666k:
+			{
+				bitTiming.prop_seg = 3;
+				bitTiming.phase_seg1 = 3;
+				bitTiming.phase_seg2 = 2;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 8;
+			}
+			break;
+			case B800k:
+			{
+				bitTiming.prop_seg = 7;
+				bitTiming.phase_seg1 = 8;
+				bitTiming.phase_seg2 = 4;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 3;
+			}
+			break;
+			case B1000k:
+			{
+				bitTiming.prop_seg = 5;
+				bitTiming.phase_seg1 = 6;
+				bitTiming.phase_seg2 = 4;
+				bitTiming.sjw = 1;
+				bitTiming.brp = 3;
+			}
+			break;
+
+			default:
+			{
+				isobus::CANStackLogger::error("[Innomaker-Windows] Unsupported baudrate with index " + std::to_string(baudrate) + " in InnomakerUSB2CANWindowsPlugin::Baudrate enum.");
+				return;
+			}
+			break;
+		}
+		driverInstance->urbSetupDevice(device, CAN_MODE, bitTiming);
+		driverInstance->openInnoMakerDevice(device);
+	}
+	else
+	{
+		isobus::CANStackLogger::error("[Innomaker-Windows] No device found on channel " + std::to_string(channel));
+	}
+}
+
+bool InnomakerUSB2CANWindowsPlugin::read_frame(isobus::HardwareInterfaceCANFrame &canFrame)
+{
+	InnoMakerUsb2CanLib::InnoMakerDevice *device = driverInstance->getInnoMakerDevice(channel);
+
+	if (nullptr == device)
+	{
+		return false;
+	}
+	BYTE receiveBuffer[sizeof(InnoMakerUsb2CanLib::innomaker_host_frame)];
+	bool success = driverInstance->recvInnoMakerDeviceBuf(device, receiveBuffer, sizeof(InnoMakerUsb2CanLib::innomaker_host_frame), 0xFFFFFFFF);
+	if (!success)
+	{
+		// Most likely a timeout
+		return false;
+	}
+	InnoMakerUsb2CanLib::innomaker_host_frame frame;
+	memcpy(&frame, receiveBuffer, sizeof(InnoMakerUsb2CanLib::innomaker_host_frame));
+
+	if (frame.echo_id != 0xFFFFFFFF)
+	{
+		InnoMakerUsb2CanLib::innomaker_tx_context *txc = driverInstance->innomaker_get_tx_context(txContexts.get(), frame.echo_id);
+		if (txc == nullptr)
+		{
+			isobus::CANStackLogger::warn("[Innomaker-Windows] Received frame with bad echo ID: " + std::to_string(static_cast<int>(frame.echo_id)));
+			return false;
+		}
+		driverInstance->innomaker_free_tx_context(txc);
+
+		/// @todo error frame handling
+		return false;
+	}
+
+	canFrame.dataLength = frame.can_dlc;
+	canFrame.isExtendedFrame = frame.can_id & CAN_EFF_FLAG;
+	if (canFrame.isExtendedFrame)
+	{
+		canFrame.identifier = frame.can_id & CAN_EFF_MASK;
+	}
+	else
+	{
+		canFrame.identifier = frame.can_id & CAN_SFF_MASK;
+	}
+	canFrame.timestamp_us = frame.timestamp_us;
+	memcpy(canFrame.data, frame.data, canFrame.dataLength);
+	return true;
+}
+
+bool InnomakerUSB2CANWindowsPlugin::write_frame(const isobus::HardwareInterfaceCANFrame &canFrame)
+{
+	InnoMakerUsb2CanLib::InnoMakerDevice *device = driverInstance->getInnoMakerDevice(channel);
+	if (nullptr == device)
+	{
+		return false;
+	}
+
+	InnoMakerUsb2CanLib::innomaker_tx_context *txc = driverInstance->innomaker_alloc_tx_context(txContexts.get());
+	if (0xff == txc->echo_id)
+	{
+		isobus::CANStackLogger::warn("[Innomaker-Windows] No free transmission context");
+		return false;
+	}
+
+	InnoMakerUsb2CanLib::innomaker_host_frame frame;
+	memset(&frame, 0, sizeof(InnoMakerUsb2CanLib::innomaker_host_frame));
+
+	frame.echo_id = txc->echo_id;
+	frame.can_dlc = canFrame.dataLength;
+	frame.can_id = canFrame.identifier;
+	memcpy(frame.data, canFrame.data, canFrame.dataLength);
+
+	if (canFrame.isExtendedFrame)
+	{
+		frame.can_id |= CAN_EFF_FLAG;
+	}
+
+	BYTE sendBuffer[sizeof(InnoMakerUsb2CanLib::innomaker_host_frame)];
+	memcpy(sendBuffer, &frame, sizeof(InnoMakerUsb2CanLib::innomaker_host_frame));
+
+	bool success = driverInstance->sendInnoMakerDeviceBuf(device, sendBuffer, sizeof(InnoMakerUsb2CanLib::innomaker_host_frame), 10);
+	if (!success)
+	{
+		isobus::CANStackLogger::warn("[Innomaker-Windows] Failed to send frame");
+		driverInstance->innomaker_free_tx_context(txc);
+	}
+	return success;
+}
