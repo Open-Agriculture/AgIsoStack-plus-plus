58e8581f3bc26a3703da3221a7fd421fd050dfbb
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2023-10-06 21:19:56 -0600

    WIP: Add VT server

--- isobus/include/isobus/isobus/isobus_virtual_terminal_server_managed_working_set.hpp
+++ isobus/include/isobus/isobus/isobus_virtual_terminal_server_managed_working_set.hpp
@@ -15,7 +15,7 @@
 #include <thread>
 
 #include "isobus/isobus/can_control_function.hpp"
-#include "isobus_virtual_terminal_objects.hpp"
+#include "isobus/isobus/isobus_virtual_terminal_objects.hpp"
 
 namespace isobus
 {
@@ -30,27 +30,16 @@ namespace isobus
 		{
 			None, ///< Thread has never been started for this working set
 			Running, ///< We are currently parsing the object pool
-			Success, ///< We have finished parsing the pool sucessfully and need to respond to the working set
+			Success, ///< We have finished parsing the pool successfully and need to respond to the working set
 			Fail, ///< The object pool is bad and we need to respond to the working set
 			Joined ///< We have sent our response to the working set master and are done parsing
 		};
 
-		/// @brief A redefinition of a common 4 component colour vector
-		class VTColourVector
-		{
-		public:
-			float x, y, z, w;
-			constexpr VTColourVector() :
-			  x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
-			constexpr VTColourVector(float _x, float _y, float _z, float _w) :
-			  x(_x), y(_y), z(_z), w(_w) {}
-		};
-
 		VirtualTerminalServerManagedWorkingSet();
-		explicit VirtualTerminalServerManagedWorkingSet(isobus::ControlFunction *associatedControlFunction);
+		VirtualTerminalServerManagedWorkingSet(std::shared_ptr<ControlFunction> associatedControlFunction);
 		~VirtualTerminalServerManagedWorkingSet();
 
-		/// @brief Starts a thread to parse the recieved object pool files
+		/// @brief Starts a thread to parse the received object pool files
 		void start_parsing_thread();
 
 		/// @brief Joins the parsing thread
@@ -62,8 +51,8 @@ namespace isobus
 		/// @returns true if at least 1 object pool has been received for this working set master, otherwise false
 		bool get_any_object_pools() const;
 
-		VTObject *get_object_by_id(std::uint16_t objectID);
-		VTObject *get_working_set_object();
+		std::shared_ptr<VTObject> get_object_by_id(std::uint16_t objectID);
+		std::shared_ptr<VTObject> get_working_set_object();
 
 		ObjectPoolProcessingThreadState get_object_pool_processing_state();
 		std::uint16_t get_object_pool_faulting_object_id();
@@ -72,9 +61,9 @@ namespace isobus
 		std::size_t get_number_iop_files() const;
 		std::vector<std::uint8_t> &get_iop_raw_data(std::size_t index);
 
-		/// @brief Returns the control funtion that is the working set master
+		/// @brief Returns the control function that is the working set master
 		/// @returns The control function that is the working set master
-		isobus::ControlFunction *get_control_function() const;
+		std::shared_ptr<ControlFunction> get_control_function() const;
 
 		/// @brief Returns the working set maintenance message timestamp
 		/// @returns The working set maintenance message timestamp in milliseconds
@@ -84,14 +73,14 @@ namespace isobus
 		/// @param[in] value New timestamp value in milliseconds
 		void set_working_set_maintenance_message_timestamp_ms(std::uint32_t value);
 
-		/// @brief Returns a colour vector associated with the specified index into the colour table
-		/// @param[in] index The index of the colour to get from the table
-		/// @returns Colour vector associated with the passed-in index
-		VTColourVector GetVTColor(std::uint8_t index) const;
+		/// @brief Saves an event callback handle for the lifetime of this object
+		/// which is useful for keeping track of callback lifetimes in a VT server
+		void save_callback_handle(std::shared_ptr<void> callbackHandle);
 
-		static constexpr std::uint16_t VT_COLOUR_TABLE_SIZE = 256; ///< The VT knows about 256 colours
-
-		std::array<VTColourVector, VT_COLOUR_TABLE_SIZE> currentColourTable; ///< Table for tracking active VT colours for this working set
+		/// @brief Returns a colour from this working set's current colour table, by index
+		/// @param[in] colourIndex The index into the VT's colour table to retrieve
+		/// @returns A colour from this working set's current colour table, by index
+		VTColourVector get_colour(std::uint8_t colourIndex) const;
 
 	private:
 		/// @brief Parses one object in the remaining object pool data
@@ -112,20 +101,19 @@ namespace isobus
 		/// @param[in] value The object ID to set as the faulting object
 		void set_object_pool_faulting_object_id(std::uint16_t value);
 
-		/// @brief Sets up the colour table to defaults from 11783-6 Annex A
-		void initialize_colour_table();
-
 		/// @brief The object pool processing thread will execute this function when it runs
 		void worker_thread_function();
 
-		std::map<std::uint16_t, VTObject *> vtObjectTree; ///< The C++ object representation (deserialized) of the object pool being managed
+		VTColourTable workingSetColourTable; ///< This working set's colour table
+		std::map<std::uint16_t, std::shared_ptr<VTObject>> vtObjectTree; ///< The C++ object representation (deserialized) of the object pool being managed
 		std::vector<std::vector<std::uint8_t>> iopFilesRawData; ///< Raw IOP File data from the client
-		std::thread *objectPoolProcessingThread; ///< A thread to process the object pool with, since that can be fairly time consuming.
-		std::mutex manangedWorkingSetMutex; ///< A mutex to protect the interface of the managed working set
-		isobus::ControlFunction *workingSetControlFunction; ///< Stores the control function associated with this working set
+		std::unique_ptr<std::thread> objectPoolProcessingThread; ///< A thread to process the object pool with, since that can be fairly time consuming.
+		std::mutex managedWorkingSetMutex; ///< A mutex to protect the interface of the managed working set
+		std::shared_ptr<ControlFunction> workingSetControlFunction; ///< Stores the control function associated with this working set
+		std::vector<std::shared_ptr<void>> callbackHandles; ///< A convenient way to associate callback handles to a working set
 		ObjectPoolProcessingThreadState processingState; ///< Stores the state of processing the object pool
 		std::uint32_t workingSetMaintenanceMessageTimestamp_ms; ///< A timestamp (in ms) to track sending of the maintenance message
-		std::uint16_t workingSetID; ///,< Stores the object ID of the working set object itself
+		std::uint16_t workingSetID; ///< Stores the object ID of the working set object itself
 		std::uint16_t faultingObjectID; ///< Stores the faulting object ID to send to a client when parsing the pool fails
 	};
 } // namespace isobus
