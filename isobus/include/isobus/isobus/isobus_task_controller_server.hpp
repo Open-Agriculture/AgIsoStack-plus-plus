4ceaf974b90bc7589c61a1976edc90ad7146a26b
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2024-01-06 15:56:12 -0700

    Add sending measurement commands, WIP

--- isobus/include/isobus/isobus/isobus_task_controller_server.hpp
+++ isobus/include/isobus/isobus/isobus_task_controller_server.hpp
@@ -10,14 +10,18 @@
 #ifndef ISOBUS_TASK_CONTROLLER_SERVER_HPP
 #define ISOBUS_TASK_CONTROLLER_SERVER_HPP
 
-#include "isobus/isobus/isobus_language_command_interface.hpp"
 #include "isobus/isobus/can_constants.hpp"
+#include "isobus/isobus/isobus_language_command_interface.hpp"
+
+#include <deque>
 
 namespace isobus
 {
 	class TaskControllerServer
 	{
 	public:
+		/// @brief Enumerates the different error codes that can be returned when activating
+		/// a device descriptor object pool.
 		enum class ObjectPoolActivationError : std::uint8_t
 		{
 			NoErrors = 0x00,
@@ -27,6 +31,8 @@ namespace isobus
 			DifferentDDOPExistsWithSameStructureLabel = 0x08
 		};
 
+		/// @brief Enumerates the different error codes that can be returned when deleting
+		/// a device descriptor object pool.
 		enum class ObjectPoolDeletionErrors : std::uint8_t
 		{
 			ObjectPoolIsReferencedByTaskData = 0,
@@ -34,6 +40,7 @@ namespace isobus
 			ErrorDetailsNotAvailable = 0xFF
 		};
 
+		/// @brief Enumerates the different error codes that can be returned when processing a DDOP
 		enum class ObjectPoolErrorCodes : std::uint8_t
 		{
 			NoErrors = 0x00,
@@ -43,6 +50,7 @@ namespace isobus
 			DDOPWasDeletedFromVolatileMemory = 0x08
 		};
 
+		/// @brief Enumerates the different process data commands that can be sent to the client.
 		enum class ProcessDataCommands : std::uint8_t
 		{
 			TechnicalCapabilities = 0x00, ///< Used for determining the technical capabilities of a TC, DL, or client.
@@ -63,11 +71,39 @@ namespace isobus
 			ClientTask = 0x0F ///< Sent by the client as a status message every 2s
 		};
 
+		/// @brief Enumerates the different options that can be reported by the server.
+		/// Each option is a bit in a bitfield, with 1 meaning the option is supported and 0 meaning it is not.
+		/// For example, if the server supports documentation and peer control assignment, but not the other options,
+		/// the bitfield would be 0b00001001.
+		enum ServerOptions : std::uint8_t
+		{
+			SupportsDocumentation = 0x01,
+			SupportsTCGEOWithoutPositionBasedControl = 0x02,
+			SupportsTCGEOWithPositionBasedControl = 0x04,
+			SupportsPeerControlAssignment = 0x08,
+			SupportsImplementSectionControl = 0x10,
+			ReservedOption1 = 0x20,
+			ReservedOption2 = 0x40,
+			ReservedOption3 = 0x80
+		};
+
+		/// @brief Constructor for a TC server.
+		/// @param internalControlFunction The control function to use to communicate with the clients.
+		/// @param numberBoomsSupported The number of booms to report as supported by the TC.
+		/// @param numberSectionsSupported The number of sections to report as supported by the TC.
+		/// @param numberChannelsSupportedForPositionBasedControl The number of channels to report as supported by the TC.
+		/// @param optionsBitfield The options bitfield to report as supported by the TC. See ServerOptions enum for the bit definitions.
 		TaskControllerServer(std::shared_ptr<InternalControlFunction> internalControlFunction,
 		                     std::uint8_t numberBoomsSupported,
 		                     std::uint8_t numberSectionsSupported,
 		                     std::uint8_t numberChannelsSupportedForPositionBasedControl,
 		                     std::uint8_t optionsBitfield);
+
+		/// @brief Deleted copy constructor
+		TaskControllerServer(TaskControllerServer &) = delete;
+
+		/// @brief Deleted assignment operator
+		TaskControllerServer &operator=(const TaskControllerServer &) = delete;
 
 		// **** Functions to be implemented by the consumer of the library ****
 		virtual bool activate_object_pool(std::shared_ptr<ControlFunction> clientControlFunction, ObjectPoolActivationError &activationError, ObjectPoolErrorCodes &objectPoolError, std::uint16_t &parentObjectIDOfFaultyObject, std::uint16_t &faultyObjectID) = 0;
@@ -98,11 +134,57 @@ namespace isobus
 
 		// **** Functions used to communicate with the client ****
 
+		/// @brief Sends a request to a client for an element's value of a particular DDI.
+		/// @param apDestination The control function to send the message to
+		/// @param aDataDescriptionIndex The Data Description Index being requested
+		/// @param aElementNumber The element number being requested
+		/// @returns true if the message was sent, otherwise false
+		bool send_request_value(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber) const;
+
+		/// @brief Sends a time interval measurement command.
+		/// The process data value for this command is the time interval for sending the data element
+		/// specified by the data dictionary identifier.The client has to send the value of this data
+		/// element to the TC or DL cyclic with this time interval.
+		/// @param clientControlFunction The control function to send the message to
+		/// @param dataDescriptionIndex The data description index of the data element to send the command for
+		/// @param elementNumber The element number of the data element to send the command for
+		/// @param timeInterval The time interval for sending the data element specified by the data dictionary identifier.
+		/// @returns true if the message was sent, otherwise false
+		bool send_time_interval_measurement_command(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t timeInterval) const;
+
+		bool send_distance_interval_measurement_command(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t distanceInterval) const;
+
+		bool send_minimum_threshold_measurement_command(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t minimum) const;
+
+		bool send_maximum_threshold_measurement_command(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t maximum) const;
+
+		bool send_change_threshold_measurement_command(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t threshold) const;
+
+		bool send_set_value_and_acknowledge(std::shared_ptr<ControlFunction> clientControlFunction, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t processDataValue) const;
+
+		/// @brief Use this to set the reported task state in the status message.
+		/// Basically, this should be set to true when the user starts a job, and false when the user stops a job.
+		/// @note Don't be like some terminals which set this to true all the time, that's very annoying for the client.
+		/// @param isTaskActive Whether a task is currently active or not.
+		void set_task_totals_active(bool isTaskActive);
+
+		/// @brief Returns whether a task is currently active or not.
+		/// @returns Whether a task is currently active or not.
+		bool get_task_totals_active() const;
+
+		/// @brief Returns the language command interface used to communicate with the client which language/units are in use.
+		/// The language command is very important for the TC to function correctly, so it is recommended that you call this
+		/// function and configure the language command interface before calling initialize().
+		/// @returns The language command interface used to communicate with the client which language/units are in use.
+		LanguageCommandInterface &get_language_command_interface();
+
+		// **** Functions used to initialize and run the server ****
+
+		/// @brief Initializes the task controller server.
 		void initialize();
 
+		/// @brief Returns whether or not the task controller server has been initialized.
 		bool get_initialized() const;
-
-		LanguageCommandInterface &get_language_command_interface();
 
 		/// @brief This must be called cyclically for the interface to operate correctly.
 		/// You can run this in a separate thread or in the main loop of your program, but
@@ -111,6 +193,7 @@ namespace isobus
 		void update();
 
 	private:
+		/// @brief Enumerates the different versions of the task controller standard.
 		enum class TaskControllerVersion : std::uint8_t
 		{
 			DraftInternationalStandard = 0, ///< The version of the DIS (draft International Standard).
@@ -121,29 +204,54 @@ namespace isobus
 			Unknown = 0xFF
 		};
 
-		/// @brief Processes messages received from task controller clients.
+		/// @brief Enumerates the different status bits that can be sent in the status message.
+		enum class ServerStatusBit : std::uint8_t
+		{
+			TaskTotalsActive = 0x01,
+			BusySavingDataToNVM = 0x02,
+			BusyReadingDataFromNVM = 0x04,
+			BusyExecutingACommand = 0x08,
+			OutOfMemory = 0x80,
+		};
+
+		/// @brief Stores messages received from task controller clients for processing later.
 		/// @param message The message received from the client.
 		/// @param parentPointer A context variable that can be used to find this class's instance.
-		static void process_rx_message(const CANMessage &message, void *parentPointer);
+		static void store_rx_message(const CANMessage &message, void *parentPointer);
+
+		/// @brief Processes messages received from task controller clients.
+		void process_rx_messages();
 
 		bool send_generic_process_data_default_payload(std::uint8_t multiplexer, std::shared_ptr<ControlFunction> destination) const;
 
+		/// @brief Sends a measurement command to the client.
+		/// @param clientControlFunction The control function to send the message to
+		/// @param commandValue The command value to send
+		/// @param dataDescriptionIndex The data description index of the data element to send the command for
+		/// @param elementNumber The element number of the data element to send the command for
+		/// @param processDataValue The process data value to send
+		/// @returns true if the message was sent, otherwise false
+		bool send_measurement_command(std::shared_ptr<ControlFunction> clientControlFunction, std::uint8_t commandValue, std::uint16_t dataDescriptionIndex, std::uint16_t elementNumber, std::uint32_t processDataValue) const;
+
+		/// @brief Sends a status message broadcast
+		/// @returns true if the message was sent, otherwise false
 		bool send_status_message() const;
 
 		static constexpr std::uint32_t STATUS_MESSAGE_RATE_MS = 2000; ///< The rate at which status messages are sent to the clients in milliseconds.
 
 		LanguageCommandInterface languageCommandInterface; ///< The language command interface used to communicate with the client which language/units are in use.
 		std::shared_ptr<InternalControlFunction> serverControlFunction; ///< The control function used to communicate with the clients.
+		std::deque<CANMessage> rxMessageQueue; ///< A queue of messages received from the clients which will be processed when update is called.
 		std::mutex taskControllerMutex; ///< A mutex used to protect the task controller server from concurrent access.
 		std::uint32_t lastStatusMessageTimestamp_ms = 0; ///< The timestamp of the last status message sent on the bus
-		const std::uint8_t numberBoomsSupportedToReport;
-		const std::uint8_t numberSectionsSupportedToReport;
-		const std::uint8_t numberChannelsSupportedForPositionBasedControlToReport;
-		const std::uint8_t optionsBitfieldToReport;
-		std::uint8_t currentStatusByte = 0;
-		std::uint8_t currentCommandByte = 0;
-		std::uint8_t currentCommandSourceAddress = NULL_CAN_ADDRESS;
-		bool initialized = false;
+		const std::uint8_t numberBoomsSupportedToReport; ///< The number of booms that will be reported as supported by the TC.
+		const std::uint8_t numberSectionsSupportedToReport; ///< The number of sections that will be reported as supported by the TC.
+		const std::uint8_t numberChannelsSupportedForPositionBasedControlToReport; ///< The number of channels that will be reported as supported by the TC.
+		const std::uint8_t optionsBitfieldToReport; ///< The options bitfield that will be reported as supported by the TC.
+		std::uint8_t currentStatusByte = 0; ///< The current status byte to send in the status message.
+		std::uint8_t currentCommandByte = 0; ///< The current command byte to send in the status message.
+		std::uint8_t currentCommandSourceAddress = NULL_CAN_ADDRESS; ///< The current command source address to send in the status message.
+		bool initialized = false; ///< Whether or not the task controller server has been initialized.
 	};
 } // namespace isobus
 
