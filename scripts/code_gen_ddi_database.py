0192b52f8e441824ce6b37d5de0371eb8d58bae4
Daan Steenbergen <d.steenbergen@student.tue.nl>
2024-01-25 19:54:40 +0100

    feat(tc): switch ddi database update script to regex based replacing

--- scripts/code_gen_ddi_database.py
+++ scripts/code_gen_ddi_database.py
@@ -1,12 +1,30 @@
 # copyright 2024 The Open-Agriculture Developers
-import os, requests
+import os, requests, re
 from datetime import datetime
+from pathlib import Path
 
-if os.path.exists("isobus_data_dictionary.cpp") and os.path.isfile("isobus_data_dictionary.cpp"):
-    os.remove("isobus_data_dictionary.cpp")
+ROOT_DIR = Path(__file__).parent.parent
+ISOBUS_SRC_DIR = ROOT_DIR / "isobus/src"
+ISOBUS_INCLUDE_DIR = ROOT_DIR / "isobus/include/isobus/isobus"
 
-if os.path.exists("isobus_data_dictionary.hpp") and os.path.isfile("isobus_data_dictionary.hpp"):
-    os.remove("isobus_data_dictionary.hpp")
+HEADER_FILE = ISOBUS_INCLUDE_DIR / "isobus_data_dictionary.hpp"
+SOURCE_FILE = ISOBUS_SRC_DIR / "isobus_data_dictionary.cpp"
+
+DDI_ARRAY_NAME = "DDI_ENTRIES"
+
+# Regex to match the DDI array, it assumes the array consists of at most depth 2 of nested braces
+DDI_ARRAY_REGEX = r"(?<=:" + DDI_ARRAY_NAME + r"\[\] = )(\{(?:[^{}]|\{(?:[^{}]|)*\})*\})"
+# Regex to match the number of DDIs in the array, it assumes there are no lookups in the
+# array where the index is a number, though lookups using variables as index is fine
+DDI_ARRAY_LENGTH_REGEX = r"(?<=" + DDI_ARRAY_NAME + r"\[)(\d+)"
+# Regex to match the file generation date
+GENERATION_DATE_REGEX = r"(?<=This file was generated )(.*)(?=\.)"
+
+if not HEADER_FILE.exists():
+    exit("Could not find header file")
+
+if not SOURCE_FILE.exists():
+    exit("Could not find source file")
 
 if os.path.exists("export.txt") and os.path.isfile("export.txt"):
     os.remove("export.txt")
@@ -25,91 +43,20 @@ with open("export.txt",'r', encoding="utf8") as f:
         if "DD Entity" in line and "Comment:" not in line:
             numberOfDDIs = numberOfDDIs + 1
 
-outputFile = open("isobus_data_dictionary.hpp", 'w', encoding="utf8")
-outputFile.write(
-f"""//================================================================================================
-/// @file isobus_data_dictionary.hpp
-///
-/// @brief This file contains the definition of an auto-generated lookup of all ISOBUS DDIs
-/// as defined in ISO11783-11, exported from isobus.net.
-/// This file was generated {datetime.today().strftime("%B %d, %Y")}.
-///
-/// @author Adrian Del Grosso
-/// @copyright {datetime.today().strftime("%Y")} The Open-Agriculture Developers
-//================================================================================================
-#ifndef ISOBUS_DATA_DICTIONARY_HPP
-#define ISOBUS_DATA_DICTIONARY_HPP
+with HEADER_FILE.open('r+', encoding="utf8") as f:
+    contents = f.read()
+    f.seek(0) # Move the cursor to the start of the file
 
-#include <cstdint>
-#include <string>
+    # Replace the generation date
+    contents = re.sub(GENERATION_DATE_REGEX, datetime.today().strftime("%B %d, %Y"), contents)
+    # Replace the number of DDIs
+    contents = re.sub(DDI_ARRAY_LENGTH_REGEX, str(numberOfDDIs), contents)
 
-namespace isobus
-{{
-	/// @brief This class contains the definition of an auto-generated lookup of all ISOBUS DDIs
-	class DataDictionary
-	{{
-	public:
-		/// @brief A struct containing the information for a single DDI
-		struct Entry
-		{{
-			const std::uint16_t ddi; ///< The DDI number
-
-			const std::string name; ///< The name of the DDI
-			const std::string units; ///< The units of the DDI
-			const float resolution; ///< The resolution of the DDI
-		}};
-
-        /// @brief Checks the ISO 11783-11 database for the given DDI number
-        /// and returns the corresponding entry if found.
-        /// @param dataDictionaryIdentifier The DDI number to look up
-        /// @return The entry for the given DDI number, or a default entry if not found
-		static const Entry &get_entry(std::uint16_t dataDictionaryIdentifier);
-
-	private:
-		static const Entry DDI_ENTRIES[{numberOfDDIs}]; ///< A lookup table of all DDI entries in ISO11783-11
-		static const Entry DEFAULT_ENTRY; ///< A default "unknown" DDI to return if a DDI is not in the database
-	}};
-}} // namespace isobus
-
-#endif // ISOBUS_DATA_DICTIONARY_HPP
+    f.write(contents)
+    f.truncate() # Remove the rest of the file (if any)
 
 
-""")
-outputFile.close()
-
-outputFile = open("isobus_data_dictionary.cpp", 'w', encoding="utf8")
-outputFile.write(
-f"""//================================================================================================
-/// @file isobus_data_dictionary.cpp
-///
-/// @brief This file contains an auto-generated lookup table of all ISOBUS DDIs as defined
-/// in ISO11783-11, exported from isobus.net.
-/// This file was generated {datetime.today().strftime("%B %d, %Y")}.
-/// @author Adrian Del Grosso
-/// @copyright {datetime.today().strftime("%Y")} The Open-Agriculture Developers
-//================================================================================================
-// clang-format off
-#include "isobus/isobus/isobus_data_dictionary.hpp"
-
-namespace isobus
-{{
-	const DataDictionary::Entry &DataDictionary::get_entry(std::uint16_t dataDictionaryIdentifier)
-	{{
-		for (std::uint_fast16_t i = 0; i < sizeof(DDI_ENTRIES) / sizeof(DataDictionary::Entry); i++)
-		{{
-			if (DDI_ENTRIES[i].ddi == dataDictionaryIdentifier)
-			{{
-				return DDI_ENTRIES[i];
-			}}
-		}}
-		return DEFAULT_ENTRY;
-	}}
-
-    const DataDictionary::Entry DataDictionary::DEFAULT_ENTRY = {{ 65535, "Unknown", "Unknown", 0.0f }};
-
-	const DataDictionary::Entry DataDictionary::DDI_ENTRIES[{numberOfDDIs}] = {{
-""")
-
+resultingArray = "{\n"
 with open("export.txt",'r', encoding="utf8") as f:
     processUnit = False
     for l_no, line in enumerate(f):
@@ -122,7 +69,7 @@ with open("export.txt",'r', encoding="utf8") as f:
                 strippedEntityLine.append(sub.replace("\n", ""))
             
             print("Processing entity", line)
-            outputFile.write(f"        {{ {strippedEntityLine[2]}, \"{strippedEntityLine[3]}\",")
+            resultingArray += f"		{{ {strippedEntityLine[2]}, \"{strippedEntityLine[3]}\", "
 
         if "Unit:" in line and processUnit:
             entityLine = line.split(' ', 1)
@@ -130,7 +77,7 @@ with open("export.txt",'r', encoding="utf8") as f:
                 entityLine[1] = "None"
             if "not defined - not defined" in entityLine[1]:
                 entityLine[1] = "None"
-            outputFile.write(f"\"{entityLine[1].strip()}\", ")
+            resultingArray += f"\"{entityLine[1].strip()}\", "
             processUnit = False
         
         if "Resolution" in line:
@@ -140,17 +87,20 @@ with open("export.txt",'r', encoding="utf8") as f:
                 entityLine[1] = '1.0'
             if '0' == entityLine[1].strip():
                 entityLine[1] = '0.0'
-            outputFile.write(f"{entityLine[1].strip()}f }}, \n")
+            resultingArray += f"{entityLine[1].strip()}f }},\n"
 
+resultingArray += "	}"
 
-f.close()
+with SOURCE_FILE.open('r+', encoding="utf8") as f:
+    contents = f.read()
+    f.seek(0) # Move the cursor to the start of the file
 
-outputFile.write(
-f"""}};
+    # Replace the generation date
+    contents = re.sub(GENERATION_DATE_REGEX, datetime.today().strftime("%B %d, %Y"), contents)
+    # Replace the number of DDIs
+    contents = re.sub(DDI_ARRAY_LENGTH_REGEX, str(numberOfDDIs), contents)
+    # Replace the DDI array
+    contents = re.sub(DDI_ARRAY_REGEX, resultingArray, contents)
 
-}} // namespace isobus
-// clang-format on
-
-""")
-
-outputFile.close()
+    f.write(contents)
+    f.truncate() # Remove the rest of the file (if any)
