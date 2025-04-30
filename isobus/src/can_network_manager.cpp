5ff58f7e200050462a471eb6e04a362f74406f69
j-c-cook <jack-c-cook@protonmail.com>
2025-04-30 11:33:31 -0500

    Format with clang-format 16

--- isobus/src/can_network_manager.cpp
+++ isobus/src/can_network_manager.cpp
@@ -1054,9 +1054,9 @@ namespace isobus
 	void CANNetworkManager::process_can_message_for_global_and_partner_callbacks(const CANMessage &message) const
 	{
 		std::shared_ptr<ControlFunction> messageDestination = message.get_destination_control_function();
-                std::shared_ptr<ControlFunction> messageSource = message.get_source_control_function();
+		std::shared_ptr<ControlFunction> messageSource = message.get_source_control_function();
 
-                if ((nullptr == messageDestination) &&
+		if ((nullptr == messageDestination) &&
 		    ((nullptr != messageSource) ||
 		     ((static_cast<std::uint32_t>(CANLibParameterGroupNumber::ParameterGroupNumberRequest) == message.get_identifier().get_parameter_group_number()) &&
 		      (NULL_CAN_ADDRESS == message.get_identifier().get_source_address()))))
@@ -1079,7 +1079,7 @@ namespace isobus
 			{
 				if ((nullptr != partner) &&
 				    (partner->get_can_port() == message.get_can_port_index()) &&
-                                    (partner == messageSource))
+				    (partner == messageSource))
 				{
 					// Message matches CAN port for a partnered control function
 					for (std::size_t k = 0; k < partner->get_number_parameter_group_number_callbacks(); k++)
